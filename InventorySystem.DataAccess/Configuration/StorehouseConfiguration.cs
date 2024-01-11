a59f87cf435bfb26cdaa2d0f3363d67eb1079c03
oscarcemagit <156161568+oscarcemagit@users.noreply.github.com>
2024-01-11 02:09:41 -0600

    Add files via upload

---
+++ InventorySystem.DataAccess/Configuration/StorehouseConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using InventorySystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventorySystem.DataAccess.Configuration
+{
+    public class StorehouseConfiguration : IEntityTypeConfiguration<Storehouse>
+    {
+        public void Configure(EntityTypeBuilder<Storehouse> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(60);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.State).IsRequired();
+        }
+    }
+}
