0c8de4981213a4d445bbf20aecececa7c90c1ef3
oscarcemagit <156161568+oscarcemagit@users.noreply.github.com>
2024-01-11 02:10:18 -0600

    Add files via upload

---
+++ InventorySystem.Models/Models/Storehouse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventorySystem.Models
+{
+    public class Storehouse
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name is required.")]
+        [MaxLength(60, ErrorMessage = "Name max length is larger as expected.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [MaxLength(100, ErrorMessage = "Description max length is larger as expected.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "State is required.")]
+        public bool State { get; set; }
+    }
+}
